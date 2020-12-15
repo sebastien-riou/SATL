85a76ffe3079d55fa673ca4da99df327230d5670
sebastien-riou <sriou@nimp.co.uk>
2020-12-16 00:20:11 +0100

    apdu cases mapping

--- test/satl_test.py
+++ test/satl_test.py
@@ -90,11 +90,23 @@ if sys.argv[1]=='slave':
     slave  = pysatl.PySatl(is_master=False,com_driver=slave_com,skip_init=skip_init)
     print("Slave init done")
 
+    def slave_spy_frame_tx(data):
+        print("SLAVE FRAME TX:",pysatl.Utils.hexstr(data))
+
+    def slave_spy_frame_rx(data):
+        print("SLAVE FRAME RX:",pysatl.Utils.hexstr(data))
+
     #slave simply replying the incoming data, length of reply indicated by P1 P2
     while True:
         cmd = slave.rx()
         #print(cmd)
         if cmd.CLA == 0xFE:
+            if cmd == pysatl.CAPDU.from_hexstr("FE 00 00 01"):
+                slave.spy_frame_tx = slave_spy_frame_tx
+                slave.spy_frame_rx = slave_spy_frame_rx
+            elif cmd == pysatl.CAPDU.from_hexstr("FE 00 00 00"):
+                slave.spy_frame_tx = None
+                slave.spy_frame_rx = None
             #in this case we just answer status word
             response = pysatl.RAPDU(0x69,0x00)
         else:
@@ -139,22 +151,69 @@ else:
     #response = master.rx()
     #print(response)
 
-    def test_lengths(lc,le,rle):
+    def test_lengths(lc,le,rle,*,print_apdu=False):
         assert(lc<=1<<16)
         assert(le<=1<<16)
         assert(rle<=le)
         lcdat=refdat[0:lc]
         capdu = pysatl.CAPDU(CLA=1,INS=rle>>16,P1=(rle>>8) & 0xFF,P2=rle & 0xFF,DATA=lcdat,LE=le)
-        #print(capdu, flush=True)
+        if print_apdu:
+            print("CAPDU hexstr:",capdu.to_hexstr(skip_long_data=True))
+            print(capdu.to_str(skip_long_data=True))
         master.tx(capdu)
         response = master.rx()
-        #print(response, flush=True)
+        if print_apdu:
+            print(response.to_str(skip_long_data=True))
+            print("RAPDU hexstr:",response.to_hexstr(skip_long_data=True))
         assert(len(response.DATA) == rle)
         assert(response.SW1 == 0x90)
         assert(response.SW2 == 0x00)
         l = min(lc,rle)
         assert(lcdat[0:l]==response.DATA[0:l])
         assert(refdatle[0:rle-l]==response.DATA[l:rle])
+
+    def master_spy_frame_tx(data):
+        dat = pysatl.Utils.hexstr(data[:12])
+        if len(data)>12:
+            dat += " "+pysatl.Utils.hexstr(data[12:],skip_long_data=True)
+        print("MASTER FRAME TX:",dat)
+
+    def master_spy_frame_rx(data):
+        if len(data)>16:
+            dat = pysatl.Utils.hexstr(data[:4]) + " " + pysatl.Utils.hexstr(data[4:-2],skip_long_data=True) + " " + pysatl.Utils.hexstr(data[-2:])
+        else:
+            dat = pysatl.Utils.hexstr(data)
+        print("MASTER FRAME RX:",dat)
+
+    def spy_apdu_case(msg,lc,le,rle=None):
+        if rle is None:
+            rle=le
+        master.spy_frame_tx = master_spy_frame_tx
+        master.spy_frame_rx = master_spy_frame_rx
+
+        print(msg)
+        test_lengths(lc,le,rle,print_apdu=True)
+
+        master.spy_frame_tx = None
+        master.spy_frame_rx = None
+
+    spy_apdu_case("CASE 1:",            0,     0)
+    spy_apdu_case("CASE 2S min:",       0,     1)
+    spy_apdu_case("CASE 2S max:",       0,   256)
+    spy_apdu_case("CASE 3S min:",       1,     0)
+    spy_apdu_case("CASE 3S max:",     255,     0)
+    spy_apdu_case("CASE 4S min:",       1,     1)
+    spy_apdu_case("CASE 4S max:",     255,   256)
+    spy_apdu_case("CASE 2E min:",       0,   257)
+    spy_apdu_case("CASE 2E max:",       0, 65536)
+    spy_apdu_case("CASE 3E min:",     256,     0)
+    spy_apdu_case("CASE 3E max:",   65535,     0)
+    spy_apdu_case("CASE 4E min 1:",   256,     1)
+    spy_apdu_case("CASE 4E min 2:",     1,   257)
+    spy_apdu_case("CASE 4E min 3:",   256,   257)
+    spy_apdu_case("CASE 4E max 1:", 65535,     1)
+    spy_apdu_case("CASE 4E max 2:",     2, 65536)
+    spy_apdu_case("CASE 4E max 3:", 65535, 65536)
 
     #for i in range(0,2):
     #    test_lengths(269,269,268)
